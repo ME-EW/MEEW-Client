b77c8c6713cb861048d926989d52ca684659abff
hellozo0 <ahju0011@gmail.com>
2021-11-21 02:58:46 +0900

    ✅[CHORE] : 데이터 설정 (#3)

--- MEEW-Client/MEEW-Client/Screens/Select/Controller/SelectVC.swift
+++ MEEW-Client/MEEW-Client/Screens/Select/Controller/SelectVC.swift
@@ -11,12 +11,16 @@ class SelectVC: BaseVC {
     
     
     @IBOutlet weak var selectCV: UICollectionView!
-    
     @IBOutlet weak var backgroundView: UIView!
+    
+    
+    var characterList:[SelectCharacterDataModel] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setUI()
+        initEventDataList()
+        
     }
     
     
@@ -24,5 +28,61 @@ class SelectVC: BaseVC {
         backgroundView.layer.cornerRadius = 8
     }
     
+//    func registerCVC() {
+//        selectCV.dataSource = self
+//        selectCV.delegate = self
+//
+//        let  = UINib(nibName: HomeEventItemCVC.identifier, bundle: nil)
+//        eventCV.register(eventitemCVC, forCellWithReuseIdentifier: HomeEventItemCVC.identifier)
+//    }
+    
+    func initEventDataList(){
+        characterList.append(contentsOf: [
+            SelectCharacterDataModel(image: "img_1", name: "귀여운 캐릭터 이름", info: "캐릭터설명 입니다. 캐릭터설명 입니다. 캐릭터설명 입니다. 캐릭터설명 입니다."),
+            SelectCharacterDataModel(image: "img_2", name: "귀여운 캐릭터 이름", info: "캐릭터설명 입니다. 캐릭터설명 입니다. 캐릭터설명 입니다. 캐릭터설명 입니다."),
+            SelectCharacterDataModel(image: "img_3", name: "귀여운 캐릭터 이름", info: "캐릭터설명 입니다. 캐릭터설명 입니다. 캐릭터설명 입니다. 캐릭터설명 입니다."),
+            SelectCharacterDataModel(image: "img_4", name: "귀여운 캐릭터 이름", info: "캐릭터설명 입니다. 캐릭터설명 입니다. 캐릭터설명 입니다. 캐릭터설명 입니다."),
+            SelectCharacterDataModel(image: "img_5", name: "귀여운 캐릭터 이름", info: "캐릭터설명 입니다. 캐릭터설명 입니다. 캐릭터설명 입니다. 캐릭터설명 입니다.")
+        ])
+     }
+    
     
 }
+
+
+// MARK: - Extension Part
+//extension SelectVC: UICollectionViewDataSource {
+//  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//      return imgName.count
+//  }
+//
+//  //indexPath에 어떤 cell 데이터를 넣을 지 결정하는 메소드
+//  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//      guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: HomeEventItemCVC.identifier, for: indexPath) as? HomeEventItemCVC else {return UICollectionViewCell()}
+//
+//      cell.setData(appData: imgName[indexPath.row])
+//      return cell
+//  }
+//}
+//
+//extension SelectVC: UICollectionViewDelegateFlowLayout {
+//  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//      let screenWidth = UIScreen.main.bounds.width
+//      let cellWidth = screenWidth
+//      let cellHeight = cellWidth * (200/375)
+//      return CGSize(width: cellWidth, height: cellHeight)
+////        return CGSize(width: 375, height: 200)
+//  }
+//
+//  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+//      UIEdgeInsets.zero
+//  }
+//
+//  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+//      0
+//  }
+//
+//  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+//      0
+//  }
+//}
