bede7f9d23c9706013509c8389bb4d1ad83bbedd
김태현 <taehyk528@gmail.com>
2022-03-11 19:46:37 +0900

    [FEAT] ReportTableViewCell 작업 (#33)

---
+++ MEEW-Client/MEEW-Client/Screens/Result/View/ReportTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ReportTableViewCell.swift
+//  MEEW-Client
+//
+//  Created by taehy.k on 2022/03/11.
+//
+
+import UIKit
+
+class ReportTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
