82e7488cb32ab0e8e92e8bd13fbd9bb233b210bc
김태현 <taehyk528@gmail.com>
2022-03-11 20:46:34 +0900

    [FEAT] 나의 기록뷰 Empty View 작업 (#33)

--- MEEW-Client/MEEW-Client/Screens/Result/Controller/ResultVC.swift
+++ MEEW-Client/MEEW-Client/Screens/Result/Controller/ResultVC.swift
@@ -11,15 +11,26 @@ final class ResultVC: BaseVC {
     
     @IBOutlet weak var todayContainerView: UIView!
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var emptyView: UIView!
+    @IBOutlet weak var todayView: UIView!
+    @IBOutlet weak var todayEmptyView: UIView!
+    
+    private var dataSourceCount: Int = 0 {
+        didSet {
+            updateUI()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+        updateUI()
         setupTableView()
     }
     
     private func setupUI() {
         todayContainerView.layer.cornerRadius = 8
+        todayEmptyView.isHidden = true
     }
     
     private func setupTableView() {
@@ -31,13 +42,24 @@ final class ResultVC: BaseVC {
         
         tableView.backgroundColor = .clear
     }
+    
+    private func updateUI() {
+        if dataSourceCount != 0 {
+            todayView.isHidden = false
+            todayEmptyView.isHidden = true
+        } else {
+            todayView.isHidden = true
+            todayEmptyView.isHidden = false
+        }
+    }
 }
 
 extension ResultVC: UITableViewDelegate {}
 
 extension ResultVC: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        3
+        emptyView.isHidden = dataSourceCount != 0
+        return dataSourceCount
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -45,7 +67,6 @@ extension ResultVC: UITableViewDataSource {
             withIdentifier: ReportTableViewCell.identifier,
             for: indexPath
         ) as? ReportTableViewCell else { return UITableViewCell() }
-        
         return cell
     }
 }
