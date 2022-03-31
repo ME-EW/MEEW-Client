760d37186f33fa6b1079ececfa91fc81c3046fae
hellozo0 <ahju0011@gmail.com>
2022-04-01 01:20:40 +0900

    🍱[ADD] : color, image asset 추가 (#36)

--- MEEW-Client/MEEW-Client/Global/Resources/Colors/Palette.swift
+++ MEEW-Client/MEEW-Client/Global/Resources/Colors/Palette.swift
@@ -34,5 +34,49 @@ extension UIColor {
   @nonobjc class var bgDarkgrey: UIColor {
     return UIColor(white: 48.0 / 255.0, alpha: 1.0)
   }
+  
+  @nonobjc class var background: UIColor {
+    return UIColor(displayP3Red: 34/255, green: 34/255, blue: 34/255, alpha: 1)
+  }
+  @nonobjc class var gray100: UIColor {
+    return UIColor(displayP3Red: 55/255, green: 55/255, blue: 55/255, alpha: 1)
+  }
+  @nonobjc class var gray200: UIColor {
+    return UIColor(displayP3Red: 73/255, green: 73/255, blue: 73/255, alpha: 1)
+  }
+  @nonobjc class var gray300: UIColor {
+    return UIColor(displayP3Red: 152/255, green: 152/255, blue: 152/255, alpha: 1)
+  }
+  @nonobjc class var gray400: UIColor {
+    return UIColor(displayP3Red: 198/255, green: 198/255, blue: 198/255, alpha: 1)
+  }
+  @nonobjc class var gray500: UIColor {
+    return UIColor(displayP3Red: 232/255, green: 232/255, blue: 232/255, alpha: 1)
+  }
+  
+  
+  @nonobjc class var purple: UIColor {
+    return UIColor(displayP3Red: 55/255, green: 55/255, blue: 55/255, alpha: 1)
+  }
+  
+  @nonobjc class var red: UIColor {
+    return UIColor(displayP3Red: 55/255, green: 55/255, blue: 55/255, alpha: 1)
+  }
+  
+  @nonobjc class var blue: UIColor {
+    return UIColor(displayP3Red: 55/255, green: 55/255, blue: 55/255, alpha: 1)
+  }
+  
+  @nonobjc class var pink: UIColor {
+    return UIColor(displayP3Red: 55/255, green: 55/255, blue: 55/255, alpha: 1)
+  }
+  
+  @nonobjc class var orange: UIColor {
+    return UIColor(displayP3Red: 55/255, green: 55/255, blue: 55/255, alpha: 1)
+  }
+  
+  @nonobjc class var yellow: UIColor {
+    return UIColor(displayP3Red: 55/255, green: 55/255, blue: 55/255, alpha: 1)
+  }
 
 }
