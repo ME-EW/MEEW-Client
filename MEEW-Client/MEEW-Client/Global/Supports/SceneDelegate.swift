eb71a04a81fb476d055c22c5b135e30974f8b624
임주민 <limj1213@gmail.com>
2022-04-07 17:58:46 +0900

    🍱[ADD] : JoinVC 파일 생성

--- MEEW-Client/MEEW-Client/Global/Supports/SceneDelegate.swift
+++ MEEW-Client/MEEW-Client/Global/Supports/SceneDelegate.swift
@@ -17,6 +17,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+      self.window = UIWindow(windowScene: scene as! UIWindowScene)
+      window?.rootViewController = JoinVC
+      window?.makeKeyAndVisible()
     }
 }
 
