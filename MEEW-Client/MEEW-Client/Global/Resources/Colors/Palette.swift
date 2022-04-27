b7129397bd623fb7be85d1cd89a5395bf411f04a
hellozo0 <ahju0011@gmail.com>
2022-04-27 19:35:42 +0900

    [ADD] color palette 수정

--- MEEW-Client/MEEW-Client/Global/Resources/Colors/Palette.swift
+++ MEEW-Client/MEEW-Client/Global/Resources/Colors/Palette.swift
@@ -35,48 +35,48 @@ extension UIColor {
     return UIColor(white: 48.0 / 255.0, alpha: 1.0)
   }
   
-  @nonobjc class var background: UIColor {
+  @nonobjc class var grey700: UIColor {
     return UIColor(displayP3Red: 34/255, green: 34/255, blue: 34/255, alpha: 1)
   }
-  @nonobjc class var gray100: UIColor {
+  @nonobjc class var gray600: UIColor {
     return UIColor(displayP3Red: 55/255, green: 55/255, blue: 55/255, alpha: 1)
   }
-  @nonobjc class var gray200: UIColor {
+  @nonobjc class var gray500: UIColor {
     return UIColor(displayP3Red: 73/255, green: 73/255, blue: 73/255, alpha: 1)
   }
-  @nonobjc class var gray300: UIColor {
+  @nonobjc class var gray400: UIColor {
     return UIColor(displayP3Red: 152/255, green: 152/255, blue: 152/255, alpha: 1)
   }
-  @nonobjc class var gray400: UIColor {
+  @nonobjc class var gray300: UIColor {
     return UIColor(displayP3Red: 198/255, green: 198/255, blue: 198/255, alpha: 1)
   }
-  @nonobjc class var gray500: UIColor {
+  @nonobjc class var gray200: UIColor {
     return UIColor(displayP3Red: 232/255, green: 232/255, blue: 232/255, alpha: 1)
   }
   
   
   @nonobjc class var purple: UIColor {
-    return UIColor(displayP3Red: 55/255, green: 55/255, blue: 55/255, alpha: 1)
+    return UIColor(displayP3Red: 173/255, green: 95/255, blue: 234/255, alpha: 1)
   }
   
   @nonobjc class var red: UIColor {
-    return UIColor(displayP3Red: 55/255, green: 55/255, blue: 55/255, alpha: 1)
+    return UIColor(displayP3Red: 231/255, green: 69/255, blue: 25/255, alpha: 1)
   }
   
   @nonobjc class var blue: UIColor {
-    return UIColor(displayP3Red: 55/255, green: 55/255, blue: 55/255, alpha: 1)
+    return UIColor(displayP3Red: 55/255, green: 154/255, blue: 252/255, alpha: 1)
   }
   
   @nonobjc class var pink: UIColor {
-    return UIColor(displayP3Red: 55/255, green: 55/255, blue: 55/255, alpha: 1)
+    return UIColor(displayP3Red: 249/255, green: 108/255, blue: 157/255, alpha: 1)
   }
   
   @nonobjc class var orange: UIColor {
-    return UIColor(displayP3Red: 55/255, green: 55/255, blue: 55/255, alpha: 1)
+    return UIColor(displayP3Red: 254/255, green: 136/255, blue: 25/255, alpha: 1)
   }
   
   @nonobjc class var yellow: UIColor {
-    return UIColor(displayP3Red: 55/255, green: 55/255, blue: 55/255, alpha: 1)
+    return UIColor(displayP3Red: 254/255, green: 227/255, blue: 40/255, alpha: 1)
   }
 
 }
