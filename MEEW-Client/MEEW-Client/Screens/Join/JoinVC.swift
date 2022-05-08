b57a9019c2f7d6059741186e87568362194c34ea
임주민 <limj1213@gmail.com>
2022-05-08 20:26:38 +0900

    ✅[CHORE] : 메인 이미지 애니메이션 적용

--- MEEW-Client/MEEW-Client/Screens/Join/JoinVC.swift
+++ MEEW-Client/MEEW-Client/Screens/Join/JoinVC.swift
@@ -10,12 +10,12 @@ import UIKit
 
 import SnapKit
 import Then
-import AuthenticationServices
 
 class JoinVC: UIViewController {
   
   // MARK: - Properties
-  let mainImageView = UIImageView()
+  var imageView = UIImageView()
+  var imageView2 = UIImageView()
   let titleLabel = UILabel()
   let subtitleLabel = UILabel()
   let kakaoButton = UIButton()
@@ -25,16 +25,49 @@ class JoinVC: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     layoutJoinView()
-    setAppleLoginButton()
+    setAnimation()
   }
   
-  private func layoutJoinView() {
-    view.backgroundColor = .black
-    view.add(mainImageView) {
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    startAnimating()
+  }
+  
+  // MARK: - Custom Func
+  func setAnimation() {
+    imageView2.transform = CGAffineTransform.identity.translatedBy(x: -self.view.bounds.width, y: 0)
+  }
+  
+  func startAnimating() {
+    UIImageView.animate(withDuration: 25, delay: 0, options: [.repeat, .curveEaseIn, .curveEaseOut], animations: {
+      self.imageView.center.x += self.view.bounds.width + 850
+      self.imageView2.center.x += self.view.bounds.width + 850
+    }, completion: nil)
+  }
+  
+  // MARK: - UI 구현
+  func layoutJoinView() {
+    view.backgroundColor = .grey700
+
+    view.add(imageView) {
+      $0.image = UIImage(named: "join_group")
       $0.snp.makeConstraints {
-        $0.centerX.equalToSuperview()
+        $0.top.equalTo(self.view.snp.top).offset(240)
+        $0.trailing.equalTo(self.view.snp.trailing).offset(-20)
+        $0.width.equalTo(1196)
+        $0.height.equalTo(140)
       }
     }
+    view.add(imageView2) {
+      $0.image = UIImage(named: "join_group")
+      $0.snp.makeConstraints {
+        $0.top.equalTo(self.view.snp.top).offset(240)
+        $0.trailing.equalTo(self.view.snp.trailing).offset(-659)
+        $0.width.equalTo(1196)
+        $0.height.equalTo(140)
+      }
+    }
+    
     view.add(titleLabel) {
       $0.text = "ME:EW"
       $0.textColor = .white
@@ -46,27 +79,25 @@ class JoinVC: UIViewController {
     }
     view.add(subtitleLabel) {
       $0.text = "또 다른 나로 살아보세요"
-      $0.textColor = .white
-      $0.font = .body3
+      $0.textColor = .grey300
+      $0.font = .body2
       $0.snp.makeConstraints {
         $0.top.equalTo(self.titleLabel.snp.bottom).offset(9)
         $0.centerX.equalToSuperview()
       }
     }
     view.add(kakaoButton) {
-      $0.setImage(UIImage(named: "kakao_login_medium_wide"), for: .normal)
+      $0.setImage(UIImage(named: "btn_kakao"), for: .normal)
       $0.snp.makeConstraints {
         $0.top.equalTo(self.view.snp.top).offset(652)
+        $0.leading.equalTo(self.view.snp.leading).offset(20)
+        $0.trailing.equalTo(self.view.snp.trailing).offset(-20)
+        $0.height.equalTo(50)
         $0.centerX.equalToSuperview()
       }
     }
-  }
-  
-  func setAppleLoginButton() {
-    let authorizationButton = ASAuthorizationAppleIDButton(type: .signIn, style: .white)
-//    authorizationButton.addTarget(self, action: #selector(handleAuthoAppleIDButnPress), for: .touchUpInside)
-    view.add(authorizationButton) {
-      authorizationButton.translatesAutoresizingMaskIntoConstraints = false
+    view.add(appleButton) {
+      $0.setImage(UIImage(named: "btn_apple"), for: .normal)
       $0.snp.makeConstraints {
         $0.top.equalTo(self.kakaoButton.snp.bottom).offset(15)
         $0.leading.equalTo(self.kakaoButton.snp.leading)
@@ -77,4 +108,3 @@ class JoinVC: UIViewController {
     }
   }
 }
-  
