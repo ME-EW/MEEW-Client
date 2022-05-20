d4f967febff921f3b52f0f4637da3eb415de084d
임주민 <limj1213@gmail.com>
2022-05-20 23:03:37 +0900

    ➕[ADD] : Asset추가, Extension추가

--- MEEW-Client/MEEW-Client/Global/Extensions/UIView+.swift
+++ MEEW-Client/MEEW-Client/Global/Extensions/UIView+.swift
@@ -33,4 +33,13 @@ extension UIView {
     layer.cornerRadius = cornerRadius
     layer.maskedCorners = CACornerMask(arrayLiteral: maskedCorners)
   }
+  func setGradient(color1: UIColor, color2: UIColor) {
+    let gradient: CAGradientLayer = CAGradientLayer()
+    gradient.colors = [color1.cgColor,color2.cgColor]
+    gradient.locations = [0.0 , 1.0]
+    gradient.startPoint = CGPoint(x: 0.0, y: 0.0)
+    gradient.endPoint = CGPoint(x: 0.0, y: 1.0)
+    gradient.frame = bounds
+    layer.addSublayer(gradient)
+  }
 }
