28827085d5714bd8700ebb4de8754df28feff1f7
hellozo0 <ahju0011@gmail.com>
2022-06-03 19:03:22 +0900

    [ADD] Loading폴더 생성

---
+++ MEEW-Client/MEEW-Client/Screens/Loading/Controller/LoadingVC.swift
@@ -0,0 +1,34 @@
+//
+//  LoadingVC.swift
+//  MEEW-Client
+//
+//  Created by 안현주 on 2022/06/03.
+//
+
+import UIKit
+
+class LoadingVC: UIViewController {
+
+  // MARK: - Vars & Lets Part
+
+
+  // MARK: - UI Component Part
+
+  
+  // MARK: - Life Cycle Part
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+         
+    }
+    
+  // MARK: - IBAction Part
+
+  
+  // MARK: - Custom Method Part
+
+  
+  // MARK: - @objc Function Part
+
+}
+// MARK: - Extension Part
