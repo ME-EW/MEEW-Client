aa71a36b746d5375f03432e27827e662455af25c
김태현 <taehyk528@gmail.com>
2022-06-08 17:16:57 +0900

    [Add] NavigationBarView

---
+++ MEEW-Client/MEEW-Client/Screens/Setting/View/NavigationBarView.swift
@@ -0,0 +1,47 @@
+//
+//  NavigationBarView.swift
+//  MEEW-Client
+//
+//  Created by taehy.k on 2022/06/08.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class NavigationBarView: UIView {
+    
+    let navigationBackButton = UIButton().then {
+        $0.setImage(UIImage(named: "icn_back"), for: .normal)
+    }
+    
+    let navigationTitleLabel = UILabel().then {
+        $0.text = "설정"
+        $0.textColor = .white
+        $0.font = UIFont(name: "SpoqaHanSansNeo-Bold", size: 18)!
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setStyle()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    private func setStyle() {
+        self.addSubviews([navigationBackButton, navigationTitleLabel])
+        
+        navigationBackButton.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview()
+            $0.leading.equalToSuperview().inset(4)
+        }
+        
+        navigationTitleLabel.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+    }
+}
