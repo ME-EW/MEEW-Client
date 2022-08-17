eb7672e83f12298c2ab29af44abb4559fbb2939a
hellozo0 <ahju0011@gmail.com>
2022-08-18 02:40:42 +0900

    [ADD] Alert ViewController 생성

---
+++ MEEW-Client/MEEW-Client/Screens/Alert/Controller/AlertViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AlertViewController.swift
+//  MEEW-Client
+//
+//  Created by 안현주 on 2022/08/18.
+//
+
+import UIKit
+
+class AlertViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
