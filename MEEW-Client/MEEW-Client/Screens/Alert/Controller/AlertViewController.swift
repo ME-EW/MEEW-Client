4535aabfe1a8a36e3eb6a95b7bbab825ea59084e
hellozo0 <ahju0011@gmail.com>
2022-08-18 02:41:28 +0900

    [CHORE] navigation bar 구현

--- MEEW-Client/MEEW-Client/Screens/Alert/Controller/AlertViewController.swift
+++ MEEW-Client/MEEW-Client/Screens/Alert/Controller/AlertViewController.swift
@@ -9,21 +9,19 @@ import UIKit
 
 class AlertViewController: UIViewController {
 
-    override func viewDidLoad() {
+	@IBOutlet weak var navigationBar: UINavigationBar!
+	
+	override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setUI()
     }
+	
+	private func setUI(){
+		navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+		navigationBar.shadowImage = UIImage()
+	}
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
