ecfb4ef08cf5eb6fd6c91281095c8d2f73c42479
김태현 <taehyk528@gmail.com>
2022-08-18 11:42:01 +0900

    ✨[FEAT] : 나의 기록 뷰(Archive View) 완성

--- MEEW-Client/MEEW-Client/Screens/Archive/Cell/ArchiveTVC.swift
+++ MEEW-Client/MEEW-Client/Screens/Archive/Cell/ArchiveTVC.swift
@@ -5,4 +5,85 @@
 //  Created by taekki on 2022/08/18.
 //
 
-import Foundation
+import UIKit
+
+final class ArchiveTVC: UITableViewCell {
+    
+    private lazy var characterImageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFill
+        $0.image = ImageLiterals.imageCharacterToday
+    }
+
+    private let progressView = ArchiveLevelProgressView()
+    
+    private let characterNameLabel = UILabel().then {
+        $0.text = "캐릭터 이름입니다."
+        $0.textColor = .white
+        $0.font = .body1
+    }
+    
+    private let timeLabel = UILabel().then {
+        $0.text = "YYYY.MM.DD"
+        $0.textColor = .grey400
+        $0.font = UIFont(name: "SpoqaHanSansNeo-Medium", size: 10.0)!
+    }
+    
+    private let lineView = UIView().then {
+        $0.backgroundColor = .grey500
+    }
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        configureUI()
+        configureLayout()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    
+    // MARK: - Configure Functions
+    
+    func configureUI() {
+        backgroundColor = .clear
+        selectionStyle = .none
+        progressView.layer.cornerRadius = 8
+    }
+
+    func configureLayout() {
+        contentView.addSubviews([characterImageView, progressView, characterNameLabel, timeLabel, lineView])
+        
+        characterImageView.snp.makeConstraints {
+            $0.width.height.equalTo(100)
+            $0.leading.equalToSuperview().inset(4)
+            $0.top.equalToSuperview().inset(10)
+        }
+        
+        progressView.snp.makeConstraints {
+            $0.leading.equalTo(characterImageView.snp.trailing).offset(10)
+            $0.top.equalToSuperview().inset(50)
+            $0.width.equalTo(88)
+            $0.height.equalTo(22)
+        }
+        
+        characterNameLabel.snp.makeConstraints {
+            $0.top.equalTo(progressView.snp.bottom).offset(8)
+            $0.leading.equalTo(progressView)
+        }
+        
+        timeLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(8)
+            $0.trailing.equalToSuperview().inset(7)
+        }
+        
+        lineView.snp.makeConstraints {
+            $0.top.equalTo(characterImageView.snp.bottom).offset(12)
+            $0.height.equalTo(0.5)
+            $0.leading.trailing.equalToSuperview()
+            $0.bottom.lessThanOrEqualToSuperview()
+        }
+    }
+}
--- MEEW-Client/MEEW-Client/Screens/Archive/Controller/ArchiveVC.swift
+++ MEEW-Client/MEEW-Client/Screens/Archive/Controller/ArchiveVC.swift
@@ -12,15 +12,26 @@ import Then
 
 final class ArchiveVC: BaseVC {
     
+    // MARK: - Properties
+    
+    var data: [Any] = ["1", "2", "3"]
+    
+    // MARK: - UI Properties
+    
     lazy var archiveBannerView = ArchiveBannerView()
     lazy var archiveHeaderView = ArchiveHeaderView()
     lazy var emptyView = ArchiveEmptyView()
+    let progressView = ArchiveLevelProgressView()
+    let tableView = UITableView().then {
+        $0.isScrollEnabled = false
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureUI()
         configureLayout()
+        configureTableView()
     }
 }
 
@@ -32,23 +43,45 @@ extension ArchiveVC {
     }
     
     private func configureLayout() {
-        view.addSubviews([archiveBannerView, archiveHeaderView, emptyView])
+        view.addSubviews([archiveBannerView, archiveHeaderView, tableView])
         
         archiveBannerView.snp.makeConstraints {
             $0.top.equalTo(view.safeAreaLayoutGuide).offset(29)
             $0.leading.trailing.equalToSuperview().inset(20)
             $0.height.equalTo(180)
         }
-        
+
         archiveHeaderView.snp.makeConstraints {
             $0.top.equalTo(archiveBannerView.snp.bottom).offset(30)
             $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(40)
         }
         
-        emptyView.snp.makeConstraints {
+        tableView.snp.makeConstraints {
             $0.top.equalTo(archiveHeaderView.snp.bottom)
-            $0.leading.trailing.equalToSuperview()
+            $0.leading.trailing.equalToSuperview().inset(20)
             $0.bottom.equalToSuperview().inset(80)
         }
+    }
+    
+    private func configureTableView() {
+        tableView.backgroundColor = .clear
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(ArchiveTVC.self, forCellReuseIdentifier: ArchiveTVC.reuseIdentifier)
+        tableView.rowHeight = UITableView.automaticDimension
+    }
+}
+
+extension ArchiveVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        tableView.backgroundView = data.isEmpty ? emptyView : nil
+        return data.isEmpty ? 0 : data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ArchiveTVC.reuseIdentifier) as! ArchiveTVC
+        return cell
     }
 }
